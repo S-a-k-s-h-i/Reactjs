59f829a52065f9a73a379781573568cab9f56431
S-a-k-s-h-i <sakshipokhria1997@gmail.com>
2021-01-13 10:02:28 +0530

    var let and const

---
+++ var_let_const.js
@@ -0,0 +1,18 @@
+let x=3,y=4,z=7;
+console.log(x,y,z);
+x=13;
+console.log(x);
+
+let a='hello';
+let _a='hy';
+let $a='hola';
+console.log(a,_a,$a);
+
+let A='namaste';
+console.log(A);
+
+const cannotChange=78;
+// cannotChange=3;
+console.log(cannotChange);
+
+// const we;
