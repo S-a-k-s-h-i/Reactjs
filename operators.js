a2d2c8d7fd0d318793c9af27280c54fff7f5d3f3
S-a-k-s-h-i <sakshipokhria1997@gmail.com>
2021-01-13 12:05:28 +0530

    Operators

---
+++ operators.js
@@ -0,0 +1,52 @@
+// Assignment operator
+let a=12
+console.log(a);
+console.log(a+=3);
+console.log(a-=5);
+console.log(a*=3);
+console.log(a/=4);
+console.log(a*=2);
+console.log(a%=4);
+console.log(a**=2);
+
+// Arithmetic operator
+let x=2;
+let y=5;
+console.log('x + y = ',x+y);
+console.log('x - y = ',x-y);
+console.log('x * y = ',x*y);
+console.log('x / y = ',x/y);
+console.log('y % x = ',y%x);
+console.log('++x = ',++x);
+console.log('x++ = ',x++);
+console.log(x)
+console.log('--x = ',--x);
+console.log('x-- = ',x--);
+console.log(x)
+console.log('x ** y = ',x**y)
+
+// Comparison operator
+console.log(4==4);
+console.log(4==='4');
+console.log(2!=2);
+console.log(2!=='2');
+console.log(7>4);
+console.log(7<4);
+
+// Logical Operators
+console.log(3<5 && 4<9);
+console.log(3<5 || 4>9);
+console.log(!true)
+
+// Bitwise operator
+console.log(2&3);
+console.log(2|3);
+console.log(2^3);
+console.log(~2);
+console.log('sakshi '+'pokhria');
+
+
+let val=(1,2,3,4);
+console.log(val);
+console.log((5>3)?'5 is greater':'3 is greater');
+
