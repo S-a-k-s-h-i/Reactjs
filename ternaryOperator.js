af2bd146c5a4e582ccf05b9c2735f89e21ce159c
S-a-k-s-h-i <sakshipokhria1997@gmail.com>
2021-01-15 07:55:01 +0530

    ternary Operator

---
+++ ternaryOperator.js
@@ -0,0 +1,11 @@
+let marks=45;
+let result=(marks>=40)?'Pass':'Fail';
+console.log(result);
+
+const age=17;
+console.log((age>=18)?'you are eligible to vote':'you are not eligible to vote');
+
+let a=-9;
+result=(a>=0)?((a=0)?'zero':'positive'):'negative';
+console.log(result);
+
