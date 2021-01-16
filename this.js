a5af34ad6f07ec01168f5258b944c42ea2174abf
S-a-k-s-h-i <sakshipokhria1997@gmail.com>
2021-01-16 23:53:52 +0530

    this

---
+++ this.js
@@ -0,0 +1,16 @@
+const obj={
+    f1(){
+        console.log(this==global);
+    },
+    f2(){
+        function f4(){
+            console.log(this);
+        }
+        return f4()
+    }
+}
+function f5(){
+    console.log(this);
+}
+const test1=obj.f1
+test1();
